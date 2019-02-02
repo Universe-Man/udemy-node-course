60cf99bd11bf271d4d35c1da0190e402640ea67d
github username <ianjpollack@gmail.com>
2019-02-01 20:01:04 -0500

    add function

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,10 +1,17 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js')
 
-let user = os.userInfo();
+// let res = notes.addNote();
+// console.log(res);
+
+let add = notes.add(9, -2);
+console.log(add);
+
+// let user = os.userInfo();
 // console.log(user);
 
 // fs.appendFile('greetings.txt', 'Hello ' + user.username + '!');
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
