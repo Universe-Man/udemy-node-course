97cfa047000ee90fdab65dabe8de8b442264c57d
github username <ianjpollack@gmail.com>
2019-02-02 16:55:40 -0500

    code clean up and nodemon installed (ready for note app development)

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,22 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js')
-
-// console.log(_.isString(true));
-// console.log(_.isString("Ian"));
-let filteredArray = _.uniq(["Ian", 1, "Ian", 1, 2, 3, 4]);
-console.log(filteredArray);
-
-
-// STUFF
-// let res = notes.addNote();
-// console.log(res);
-// let add = notes.add(9, -2);
-// console.log(add);
-// let user = os.userInfo();
-// console.log(user);
-// fs.appendFile('greetings.txt', 'Hello ' + user.username + '!');
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
