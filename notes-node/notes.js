0bdcc6cde9176870af42185beb6038f65a43b8d4
github username <ianjpollack@gmail.com>
2019-02-15 12:22:33 -0500

    yargs done

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,25 @@
 console.log('Starting notes.js');
 
-// console.log(module);
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+let addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+let getAll = () => {
+  console.log('Getting all notes');
+};
+
+let getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+let removeNote = (title) => {
+  console.log('Removing note', title);
+}
+
+module.exports = {
+  // addNote: addNote
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
 }
